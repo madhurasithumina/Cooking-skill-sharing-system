11ee519ee1f720a322fcf86c08346452972d363c
madhurasithumina <128403933+madhurasithumina@users.noreply.github.com>
2025-05-01 23:23:08 +0530

    initial commit

---
+++ cooking-skill-backend/src/test/java/com/cookingskill/cooking_skill_backend/CookingSkillBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookingskill.cooking_skill_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingSkillBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
