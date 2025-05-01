c8ddc8ed65bdfe6cd68ceeb7d5f5747d2a83e73a
madhurasithumina <128403933+madhurasithumina@users.noreply.github.com>
2025-05-02 03:34:52 +0530

    update app.js

--- frontend-new/src/App.js
+++ frontend-new/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage';
+import UserForm from './components/UserForm/UserForm';
+import LoginForm from './components/LoginForm/LoginForm';
+import UserProfile from './components/UserProfile/UserProfile';
+import HomePage from './components/HomePage/HomePage';
+import WorkPlanForm from './components/WorkPlanForm/WorkPlanForm';
+import WorkPlanList from './components/WorkPlanList/WorkPlanList';
+import WorkPlanEdit from './components/WorkPlanEdit/WorkPlanEdit';
+import PostForm from './components/PostForm/PostForm';
+import PostList from './components/PostList/PostList';
+import PostEdit from './components/PostEdit/PostEdit';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<UserForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/profile/:userId" element={<UserProfile />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/workplan/create" element={<WorkPlanForm />} />
+          <Route path="/workplan/list" element={<WorkPlanList />} />
+          <Route path="/workplan/edit/:id" element={<WorkPlanEdit />} />
+          <Route path="/post/create" element={<PostForm />} />
+          <Route path="/post/list" element={<PostList />} />
+          <Route path="/post/edit/:id" element={<PostEdit />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
